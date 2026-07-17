6aebc705e6d106614ff7c3e9d95efd4290a9f7f7
delaney <delaney.lothian@gmail.com>
2026-07-17 15:57:57 -0600

    actions create or reuse R2 bucket

--- src/app/protected/protected.ts
+++ src/app/protected/protected.ts
@@ -9,4 +9,19 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
 })
 export class ProtectedComponent {
   constructor(public auth: AuthService) { }
+
+  selectedFile?: File;
+
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+
+      // Later:
+      // upload this.selectedFile to your Worker
+      console.log("Uploaded file:", this.selectedFile);
+    }
+  }
 }
